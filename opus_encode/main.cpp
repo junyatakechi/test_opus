faa5ed6dd3c0ac73f1c8d9e7c4d20d544c21b822
junyatakechi <takechi48j@gmail.com>
2020-02-22 22:02:01 +0900

    Initial Commit

---
+++ opus_encode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opus_encode
+//
+//  Created by Takechi on 2020/02/22.
+//  Copyright © 2020年 junyatakechi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
